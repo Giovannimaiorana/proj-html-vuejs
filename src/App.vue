b36c4dfdcda0ee24b3ee9033565f7c3255eb365d
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-04-11 17:21:35 +0200

    fine struttura Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
-<script setup>
+<script>
+import ThisHeader from './components/TheHeader.vue'
+export default {
+  components: {
+    ThisHeader,
+  }
+}
 
 </script>
 
-<template></template>
+<template>
+  <header>
+    <ThisHeader />
+  </header>
+  <main>
 
-<style></style>
+  </main>
+  <footer>
+
+  </footer>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,143 @@
+<script>
+export default {
+    name: 'ThisHeader',
+
+}
+</script>
+
+<template>
+    <div class="containerHeader">
+        <div class="contentHeader">
+            <!--CONTENUTO HEADER DI JUMBOTRON INIZIO-->
+            <div class="headerContent">
+                <div class="logo">
+                    <img src="../assets/dark-logo.png" alt="">
+                </div>
+                <div class="navlink">
+                    <ul>
+                        <li>Home <i class="fa-solid fa-chevron-down"></i></li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                    </ul>
+                </div>
+                <div class="social">
+                    <span><i class="fa-brands fa-twitter"></i></span>
+                    <span><i class="fa-brands fa-facebook-f"></i></span>
+                    <span><i class="fa-brands fa-instagram"></i></span>
+                    <span><i class="fa-brands fa-linkedin-in"></i></span>
+
+                </div>
+            </div>
+            <!--CONTENUTO HEADER DI JUMBOTRON FINE-->
+            <!--CONTENUTO HEADER DI JUMBOTRON SECONDA PARTE INIZIO-->
+            <div class="containerJumboBott">
+                <div class="descriptionContainerBottom">
+                    <p>I'M WINGAM</p>
+                    <h1>Peace On Heart A Wonderful Wish But No Way</h1>
+                    <p>Success isn't overnight. It's when everyday you get a little better than the day before. It all adds
+                        up.</p>
+                    <button>Get Started For Free <i class="fa-solid fa-arrow-right"></i></button>
+                </div>
+                <div class="containerPhoto">
+                    <img src="../assets/home-movation-hero-image.png" alt="">
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped lang="scss">
+.containerHeader {
+    max-width: 100%;
+    height: 600px;
+    background-color: rgb(236, 233, 237);
+    background-image: url(../assets/background-pattern-wavify.png);
+}
+
+.contentHeader {
+    max-width: 1300px;
+    margin: auto;
+}
+
+.headerContent {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+
+    .logo {
+        img {
+            width: 150px;
+        }
+    }
+
+    .navlink {
+        ul {
+            display: flex;
+            list-style: none;
+
+
+            li {
+                margin-left: 10px;
+
+
+                i {
+                    font-size: 10px;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+
+    .social {
+        span {
+            margin-left: 10px;
+        }
+    }
+}
+
+/*STILE JUMBOTROM BOTTOM*/
+.containerJumboBott {
+    display: flex;
+    justify-content: center;
+
+
+    .descriptionContainerBottom {
+        width: 400px;
+
+        p {
+            margin-top: 70px;
+            margin-bottom: 30px;
+            line-height: 30px;
+
+        }
+
+        button {
+            height: 40px;
+            width: 250px;
+            padding: 10px;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            background-color: rgb(34, 173, 150);
+        }
+    }
+
+    .containerPhoto {
+        width: 400px;
+        margin-left: 300px;
+
+        img {
+            position: absolute;
+            top: 113px;
+            width: 400px;
+        }
+    }
+}
+</style>
